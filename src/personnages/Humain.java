9a3346c67954722cdc4a434a70a11b07db7d8381
Raphael Leguevaques <lgr5307a@ensfsi.onmicrosoft.com>
2026-04-16 11:23:08 +0200

    les humains ont des connaissances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,6 +4,9 @@ public class Humain {
 	private String nom;
 	private int argent;
 	private String boisson_favorite;
+	protected int nbConnaissance= 0;
+	private int MEMOIRE_MAX = 3;
+	protected Humain[] memoire  = new Humain[MEMOIRE_MAX];
 	
 	public Humain(String nom, String boisson_favorite, int argent) {
 		this.nom = nom;
@@ -47,4 +50,40 @@ public class Humain {
 			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous." );
 		}
 	}
+	private void repondre(Humain humain) {
+		direBonjour();
+		memoriser(humain);
+	}
+	
+	private void memoriser(Humain humain) {
+		if(nbConnaissance < MEMOIRE_MAX) {
+			memoire[nbConnaissance] = humain;
+			nbConnaissance++;
+		}
+		else {
+			for(int i = 0; i < MEMOIRE_MAX-1; i++) {
+				memoire[i] = memoire[i+1];
+			}
+			memoire[MEMOIRE_MAX-1] = humain;
+		}
+	}
+	public void faireConnaissanceAvec(Humain autreHumain) {
+		direBonjour();
+		autreHumain.repondre(this);
+		memoriser(autreHumain);
+		
+	}
+	
+	public void listerConnaissance() {
+		String texte = "Je connais beaucoup de monde dont : ";
+	
+		for (int i = 0; i < nbConnaissance; i++) {
+			texte += memoire[i].getNom();
+			if(i< nbConnaissance-1) {
+				texte +=", ";
+			}
+		}
+		parler(texte);
+	}
+	
 }
